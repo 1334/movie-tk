c0d18b04bc998e85535341aeaa45553aa92a6acc
1334 <1334@shadowproject.net>
2013-02-11 20:29:45 +0100

    make movies integration tests pass

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,25 @@
+require 'omdb/client'
+
 class MoviesController < ApplicationController
   http_basic_authenticate_with :name => ENV['HTTP_USER'], :password => ENV['HTTP_PASS']
 
   def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    movie = Omdb::Client.search_by_imdb_id params["movie"]["imdb_id"]
+    if movie.is_a? Omdb::MovieNotFound
+      @movie = Movie.new imdb_id: params["movie"]["imdb_id"]
+      @movie.errors.add(:imdb_id, "movie not found")
+      render action: "new"
+    else
+      Movie.new_from_omdb movie
+      redirect_to movies_path, alert: "Movie added successfully"
+    end
   end
 end
--- test/integration/movies_test.rb
+++ test/integration/movies_test.rb
@@ -2,8 +2,19 @@ require 'test_helper'
 
 class MoviesTest < ActionDispatch::IntegrationTest
   test "list no movies" do
+    Movie.delete_all
+
     visit_and_authenticate movies_path
 
     assert page.has_content?('There are no movies'), "Message 'There are no movies' not found on page"
+  end
+
+  test "can add a movie from imdb_id" do
+    visit_and_authenticate new_movie_path
+    fill_in :movie_imdb_id, with: "tt1139328"
+    click_on "Create"
+
+    assert_equal movies_path, current_path
+    assert page.has_content?('The Ghost Writer'), "Page doesn't have text 'The Ghost Writer'"
   end
 end
