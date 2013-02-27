9f0eec90862e98aae2f9f0fb9760c95bdad745a6
1334 <1334@shadowproject.net>
2013-02-27 20:08:06 +0100

    add funcionality to search by movie title

--- app/services/omdb/client.rb
+++ app/services/omdb/client.rb
@@ -7,15 +7,30 @@ module Omdb
       new.search_by_imdb_id id
     end
 
+    def self.serach_by_title title
+      new.serach_by_title title
+    end
+
     def search_by_imdb_id id
-      response = JSON.parse get(id)
+      response = search "i=#{id}"
       response["Response"] == "False" ? MovieNotFound.new : response
+    end
+
+    def serach_by_title title
+      # omdbapi returns a Hash  with 'Search' => results_array 
+      # by omdbapi design it returns a maximum of 10 results
+      response = search "s=#{title}"
+      response["Response"] == "False" ? MovieNotFound.new : response['Search']
     end
 
     private
 
+    def search query
+      response = JSON.parse get(query)
+    end
+
     def get query
-      open("http://www.omdbapi.com/?i=#{URI::encode query}").read
+      open("http://www.omdbapi.com/?#{URI::encode query}").read
     end
   end
 
--- test/unit/omdb_client_test.rb
+++ test/unit/omdb_client_test.rb
@@ -31,5 +31,27 @@ module Omdb
         end
       end
     end
+
+    describe '#search_by_title' do
+      describe 'no results' do
+        it 'returns a MovieNotFound instance' do
+          VCR.use_cassette('no_movies_matching_this_string') do
+            response = Client.serach_by_title 'no_movies_matching_this_string'
+            response.class.must_equal Omdb::MovieNotFound
+          end
+        end
+      end
+
+      describe 'with some results' do
+        it 'returns the correct data' do
+          VCR.use_cassette('star_wars') do
+            response = Client.serach_by_title 'star wars'
+            response.class.must_equal Array
+            response.count.must_equal 10
+            response.select {|r| r['imdbID'] == "tt0076759" }.count.must_equal 1
+          end
+        end
+      end
+    end
   end
 end
